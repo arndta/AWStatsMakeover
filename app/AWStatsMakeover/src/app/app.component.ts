209cd0d474d7d984ce934121a6c93dd1ecc65995
Andy Arndt <aarndt@gmail.com>
2018-10-31 18:23:32 -0500

    Angular app (#5)
    * initial angular template
    
    * config and filelist services
    
    * config and filelist services
    
    * added dates service, home routing
    
    * added site data service to load data files

--- app/AWStatsMakeover/src/app/app.component.ts
+++ app/AWStatsMakeover/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ConfigService } from './data/config/config.service';
 import { FileListService } from './data/filelist/filelist.service';
+import { SiteDataService } from './data/sitedata/sitedata.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,9 @@ import { FileListService } from './data/filelist/filelist.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(configService : ConfigService, fileListService : FileListService) {
+  constructor(configService : ConfigService, fileListService : FileListService, siteDataService : SiteDataService) {
     configService.load();
     fileListService.load();
+    siteDataService.load();
   }
 }
---
+++ app/AWStatsMakeover/src/app/shared/interface/IDataListing.ts
@@ -0,0 +1,5 @@
+export interface IDataListing {
+    year: number;
+    month: number;
+    data: any;
+}
